a6244e0c50d4eb255bc5fec9cd19a5a9cdffa6e6
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-12 23:09:03 +0000

    API getting png

--- avatarAPI.js
+++ avatarAPI.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secret = require('./secret.js')
+var fs = require('fs');
 console.log('Welcome to Avatar API');
 
 request.get('https://api.github.com/repos/jquery/jquery/contributors');
@@ -18,10 +19,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
     });
   }
   
+   function downloadImageByURL(url, filePath) {
 
+     request.get(url)
+     .pipe(fs.createWriteStream(filePath));
+ }
+ downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', "./test.png");
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+    result.forEach( function(element) {
+      console.log(element);
+    })
+     console.log("Errors:", err);
+     console.log("Result:", result);
   });
 
+ 
