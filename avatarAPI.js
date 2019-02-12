4e5a34d9949a153e2ee82a664c95cd4ea55739c8
CristofMorissette1 <cristofmorissette@hotmail.com>
2019-02-12 21:17:59 +0000

    Avatar files updated

--- avatarAPI.js
+++ avatarAPI.js
@@ -1,3 +1,27 @@
 var request = require('request');
+var secret = require('./secret.js');
+console.log('Welcome to Avatar API');
 
-console.log('This is an avatar API')
+request.get('https://api.github.com/repos/jquery/jquery/contributors');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request'
+        }
+      };
+    
+    request(url, function(err, res, body) {
+      cb(err, body);
+    });
+  }
+  
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
+
---
+++ secret.js
@@ -0,0 +1,3 @@
+module.exports = {
+    GITHUB_TOKEN: "9a70d8ddbadd9dc396573cb301f4dd9800669048"
+  };
